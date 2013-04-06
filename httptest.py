f911085252797a8e89b3e29a3d8b05a09b3ca854
Alexander Bykov <bykov@corp.mail.ru>
2013-04-06 15:12:44 +0400

    fix HEAD check, support more JS content-types

--- httptest.py
+++ httptest.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
+import re
+import socket
 import httplib
 import unittest
 
 class HttpServer(unittest.TestCase):
-  conn = None
+  host = "localhost"
+  port = 80
 
   def setUp(self):
-    self.conn = httplib.HTTPConnection("localhost", 80, timeout=10)
+    self.conn = httplib.HTTPConnection(self.host, self.port, timeout=10)
 
   def tearDown(self):
     self.conn.close()
@@ -123,15 +126,32 @@ class HttpServer(unittest.TestCase):
 
   def test_head_method(self):
     """head method support"""
-    self.conn.request("HEAD", "/httptest/dir2/page.html")
-    r = self.conn.getresponse()
-    data = r.read()
-    length = r.getheader("Content-Length")
-    if int(r.status) == 200:
-      self.assertEqual(int(length), 38)
-      self.assertEqual(len(data), 0)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((self.host, self.port))
+    s.send("HEAD /httptest/dir2/page.html HTTP/1.0\r\n\r\n")
+    data = ""
+    while 1:
+      buf = s.recv(1024)
+      if not buf: break
+      data += buf
+    s.close()
+
+    self.assertTrue(data.find("\r\n\r\n") > 0, "no empty line with CRLF found")
+    (head, body) = re.split("\r\n\r\n", data, 1);
+    headers = head.split("\r\n");
+    self.assertTrue(len(headers) > 0, "no headers found")
+    statusline = headers.pop(0)
+    (proto, code, status) = statusline.split(" ");
+    h = {}
+    for k,v in enumerate(headers):
+      (name, value) = re.split('\s*:\s*', v, 1)
+      h[name] = value
+    if (int(code) == 200):
+      self.assertEqual(int(h['Content-Length']), 38)
+      self.assertEqual(len(body), 0)
     else:
-      self.assertIn(int(r.status), (400,405))
+      self.assertIn(int(code), (400,405))
 
   def test_filetype_html(self):
     """Content-Type for .html"""
@@ -168,6 +188,7 @@ class HttpServer(unittest.TestCase):
     self.assertEqual(int(length), 268381)
     self.assertEqual(len(data), 268381)
     self.assertEqual(ctype, "application/x-javascript")
+    self.assertIn(ctype, ("application/x-javascript", "application/javascript", "text/javascript"))
 
   def test_filetype_jpg(self):
     """Content-Type for .jpg"""
